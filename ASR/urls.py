8e1249a3699ee4d1784b9a23150f1118b85b3c9d
Georg Avagyan <georgavagyan@gmail.com>
2020-11-24 11:47:32 +0500

    Логин/логаут добавлены
    Если вход не выполнен, профиль недоступен

--- ASR/urls.py
+++ ASR/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from .views import settings, notifications_settings, sign_in, sign_up, profile
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('notifications_settings', notifications_settings,
          name='notifications_settings'),
     path('profile', profile, name='profile'),
