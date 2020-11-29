373f90d65d3e1d30aae2f7309952bfe6f3299c13
GreatKing4615 <59148289+GreatKing4615@users.noreply.github.com>
2020-11-29 19:37:51 +0500

    add_events

--- ASR/views.py
+++ ASR/views.py
@@ -1,5 +1,7 @@
 # from json import load
 from django.shortcuts import render
+import caldav as caldav
+import datetime
 # from django.core import serializers
 # from django.http import JsonResponse
 
@@ -19,7 +21,31 @@ def notifications_settings(request):
 
 
 def sign_in(request):
-    return render(request, 'sign_in.html')
+    #  в консоли это работает
+    client = caldav.DAVClient(url='https://caldav.yandex.ru/',
+                              username='test@aventica.ru', password='jaujzhfpfvynifqv',
+                              )
+    my_principal = client.principal()
+    calendars = my_principal.calendars()
+
+    calendars[0].save_event("""BEGIN:VCALENDAR
+    VERSION:2.0
+    PRODID:-//Example Corp.//CalDAV Client//EN
+    BEGIN:VEVENT
+    UID:20200516T060000Z-test@aventica.ru
+    DTSTAMP:20201125T060000Z
+    DTSTART:20201125T060000Z
+    DTEND:20201125T230000Z
+    RRULE:FREQ=YEARLY
+    SUMMARY:1234
+    END:VEVENT
+    END:VCALENDAR
+    """)
+
+
+
+    # return render(request, 'sign_in.html')
+
 
 
 def sign_up(request):
