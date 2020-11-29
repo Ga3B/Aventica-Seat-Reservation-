d8e25083ec73d6f3aaf1a7860a259f7bd30d5cf4
Georg Avagyan <georgavagyan@gmail.com>
2020-11-29 19:36:16 +0500

    Смена часовых поясов в профиле

--- ASR/views.py
+++ ASR/views.py
@@ -1,13 +1,10 @@
-# from json import load
+from json import dumps
 from django.shortcuts import render
 from MainApp.models import User_preferences
 from MainApp.forms import User_preferencesFrom
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib import messages
 from django.urls import reverse
-# from django.http import JsonResponse
-
-# Create your views here.
 
 
 def settings(request):
@@ -15,6 +12,28 @@ def settings(request):
 
 
 def profile(request):
+    if request.is_ajax and request.method == "POST":
+        timezones = [
+            'Europe/Kaliningrad, UTC+02:00',
+            'Europe/Moscow, UTC+03:00',
+            'Europe/Volgograd, UTC+04:00',
+            'Asia/Yekaterinburg, UTC+05:00',
+            'Asia/Omsk, UTC+06:00',
+            'Asia/Krasnoyarsk, UTC+07:00',
+            'Asia/Irkutsk, UTC+08:00',
+            'Asia/Yakutsk, UTC+09:00',
+            'Asia/Vladivostok, UTC+10:00',
+            'Asia/Sakhalin, UTC+11:00',
+            'Asia/Kamchatka, UTC+12:00'
+        ]
+        new_timezone = request.POST.get('ntz', '')
+        if new_timezone not in timezones:
+            return JsonResponse({"error": "Invalid timezone"}, status=400)
+        ups = request.user.user_preferences_set.all()[0]
+        ups.timezone = new_timezone
+        ups.save()
+        return JsonResponse(dumps(f'timezone changed to {new_timezone}'), safe=False, status=200)
+
     timezone = request.user.user_preferences_set.all()[0].timezone
     return render(request, "profile.html", {"timezone": timezone})
 
