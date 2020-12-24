5f41b100a8ac0d26b0469ffdae6a17fd58c2e82f
Georg Avagyan <georgavagyan@gmail.com>
2020-12-24 15:29:15 +0500

    Update settings.py

--- ASR/settings.py
+++ ASR/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = 'MainApp:index'
 LOGOUT_REDIRECT_URL = 'MainApp:index'
